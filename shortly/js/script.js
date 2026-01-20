719dc707f0c9f550a0163d1af39ff2804971fa18
Victor Kovalchuk <vict02r05@gmail.com>
2026-01-21 00:05:55 +0200

    mobile menu

--- shortly/js/script.js
+++ shortly/js/script.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.getElementById('menuBtn');
+  const menu = document.getElementById('menu');
+  const body = document.body;
+
+  menuBtn.addEventListener('click', navToggle);
+  
+  function navToggle () {
+    menuBtn.classList.toggle('open');
+    menu.classList.toggle('flex');
+    menu.classList.toggle('hidden');
+    body.classList.toggle('overflow-hidden');
+  }
+  
+
   const input = document.getElementById('link-input');
   const linkForm = document.getElementById('link-form');
   const errMsg = document.getElementById('err-msg');
