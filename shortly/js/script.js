29e62098fa035e5c485b5b48fcc546c3790ab4b5
Victor Kovalchuk <vict02r05@gmail.com>
2026-01-20 23:13:16 +0200

    Java Script Link Validation

---
+++ shortly/js/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const input = document.getElementById('link-input');
+  const linkForm = document.getElementById('link-form');
+  const errMsg = document.getElementById('err-msg');
+
+  linkForm.addEventListener('submit', formSubmit);
+
+  function validURL(str) {
+    var pattern = new RegExp(
+      '^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+        '((\\d{1,3}\\.){3}\\d{1,3}))' +
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+        '(\\?[;&a-z\\d%_.~+=-]*)?' +
+        '(\\#[-a-z\\d_]*)?$',
+      'i'
+    )
+    return !!pattern.test(str)
+  }
+
+  function formSubmit (e) {
+    e.preventDefault();
+    
+    if (input.value === '') {
+      errMsg.textContent = 'Please enter something';
+      input.classList.add('border-red');
+    } else if (!validURL(input.value)) {
+      errMsg.textContent = 'Please enter a valid URL';
+      input.classList.add('border-red');
+    } else {
+      errMsg.textContent = '';
+      input.classList.remove('border-red');
+      linkForm.submit();
+    }
+  }
+});
